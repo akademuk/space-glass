196941d8a7a44a59cb2c15581a4ee00f7fcb686f
akademuk <akademuk24@gmail.com>
2023-03-01 13:44:22 +0200

    ss

---
+++ script/otherProjects.js
@@ -0,0 +1,39 @@
+$('.otherProjectsSlider').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots:true,
+    arrows: false,
+    appendDots: $('.otherProjectsSliderDots'),
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2.5,
+            slidesToScroll: 1,
+          },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1.5,
+              slidesToScroll: 1,
+            },
+          },
+      ],
+});
+      
